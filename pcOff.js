da3002afd623b869545591f06aa2606f9bc8f3b7
paxagi <paxagi@gmail.com>
2022-01-18 22:44:23 +0300

    fix: use only one pcOff file

---
+++ pcOff.js
@@ -0,0 +1,23 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec)
+
+// запуск команды (в cmd?)
+async function runCommand(command) {
+  const { stdout, stderr, error } = await exec(command)
+  if (stderr) { console.error('stderr:', stderr) }
+  if (error) { console.error('error:', error) }
+  return stdout; // стандартный вывод
+}
+
+const commands = {
+  shutdown: (compName) => `shutdown /s -t 1 -m \\\\${compName}`,
+}
+
+async function pcoff(compName) {
+  const command = commands.shutdown(compName)
+  const result = await runCommand(command)
+}
+module.exports = {
+  pc1off: pcoff
+}
+
