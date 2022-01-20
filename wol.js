8d8de7ffa3b640949c4cc2bdfd173cfba41be3c6
paxagi <paxagi@gmail.com>
2022-01-20 08:30:31 +0300

    wol.js refactoring

--- wol.js
+++ wol.js
@@ -1,19 +1,11 @@
-﻿var script1 = require('./pc1off.js');
-var script2 = require('./pc2off.js');
-var script5 = require('./pc5off.js');
-var script7 = require('./pc7off.js');
-var script8 = require('./pc8off.js');
-var script9 = require('./pc9off.js');
-var script11 = require('./pc11off.js');
-
+﻿const wol = require('wake_on_lan');
 const pcoff = require('./pcOff')
-
-return
-
 const TelegramBot = require('node-telegram-bot-api');
+
 const token = '';
 
 const bot = new TelegramBot(token, { polling: true });
+
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
@@ -21,6 +13,8 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 })
 
 bot.on('message', (msg) => {
+  const chatId = msg.chat.id
+
   const MACs = {
     1: '',
     2: '',
@@ -32,105 +26,39 @@ bot.on('message', (msg) => {
     15: '',
   }
 
-  const chatId = msg.chat.id
-
-  if (msg.text.toLowerCase() === 'рестарт') {
-    bot.sendMessage(chatId, "Это еще не реализовано")
-  }
-  // ПК 1 **********************************************************
-  else if (msg.text.toLowerCase() === 'бот включи комп 1') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  }
-  else if (msg.text.toLowerCase() === 'бот выключи комп 1') {
-    bot.sendMessage(chatId, "Выключаю комп 1")
-    script1.pcoff(compId);
-  } // ПК 1 ------------------------------------------------------------
-
-
-
-
-  // ПК 2 **********************************************************
-  else if (msg.text.toLowerCase() === 'бот включи комп 2') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  }
-  else if (msg.text.toLowerCase() === 'бот выключи комп 2') {
-    bot.sendMessage(chatId, "Выключаю комп 2")
-    script2.pc2off();
-  } // ПК 2 ------------------------------------------------------------
-
-  //ПК 5 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 5') {
-    bot.sendMessage(chatId, "Выключаю комп 5")
-    script5.pc5off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 5') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 5 -------------------------------------------------------------
-
-  //ПК 7 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 7') {
-    bot.sendMessage(chatId, "Выключаю комп 7")
-    script7.pc7off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 7') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 7 -------------------------------------------------------------
-
-  //ПК 8 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 8') {
-    bot.sendMessage(chatId, "Выключаю комп 8")
-    script8.pc8off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 8') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 8 -------------------------------------------------------------
-
-  //ПК 9 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 9') {
-    bot.sendMessage(chatId, "Выключаю комп 9")
-    script9.pc9off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 9') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 9 -------------------------------------------------------------
-
-  //ПК 11 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 11') {
-    bot.sendMessage(chatId, "Выключаю комп 11")
-    script11.pc11off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 11') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 11 -------------------------------------------------------------
-
-  //ПК 15 ***********************************************************
-  else if (msg.text.toLowerCase() === 'бот выключи комп 15') {
-    bot.sendMessage(chatId, "Выключаю комп 15")
-    script11.pc11off();
-  }
-  else if (msg.text.toLowerCase() === 'бот включи комп 15') {
-    bot.sendMessage(chatId, "Минутку, включаю...")
-    var wol = require('wake_on_lan');
-    wol.wake('');
-  } //ПК 15 -------------------------------------------------------------
-
-  else if (msg.text.toLowerCase() === 'бот') {
-    bot.sendMessage(chatId, "Я тут")
+  const tgUsersCommandsTree = {
+    'бот': {
+      'включи': {
+        'комп': num => {
+          if (MACs[mun]) {
+            bot.sendMessage(chatId, "Минутку, включаю...")
+            wol.wake(MACs[num])
+          } else {
+            bot.sendMessage(chatId, "ПК с таким номером нет")
+          }
+        },
+      },
+    }
   }
 
-  //	process.exit();
+
+  function executeTextCommand(text) {
+    const words = text.split(' ')
+    let parent = tgUsersCommandsTree
+    for (const word of words) {
+      parent = parent[word]
+      if (typeof parent === 'function') parent = parent(word)
+    }
+
+    return result = parent
+  }
+
+  /** executing */
+
+  const text = msg.text
+
+  const res = executeTextCommand(text)
+
+  console.log(res)
+
 })
