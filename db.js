8a2d879555922e38eb3207d8472debfbec48d292
paxagi <paxagi@gmail.com>
2022-01-23 19:51:04 +0300

    feat: add "date base" file

---
+++ db.js
@@ -0,0 +1,28 @@
+const skirko = [1, 2]
+const manegers = [7, 9, 11]
+const enginears = [5, 8, 15]
+
+module.export = {
+  users: [
+    {
+      username: 'skirko-vn',
+      tgUserId: null,
+      access: 'all'
+    },
+    {
+      username: 'rozevskaya-v?',
+      tgUserId: null,
+      access: manegers
+    },
+    {
+      username: 'arafailov-as',
+      tgUserId: 171549268,
+      access: 11,
+    },
+    {
+      username: 'mamedov-rn',
+      tgUserId: 825873412,
+      access: 7,
+    },
+  ]
+}
